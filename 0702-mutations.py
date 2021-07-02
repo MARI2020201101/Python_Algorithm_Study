1438e9e564b1c0789408a47eb09a49ba849668ed
MARI <mari20201101@gmail.com>
2021-07-02 17:45:51 +0900

    Try hackerrank mutations

---
+++ 0702-mutations.py
@@ -0,0 +1,13 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def mutate_string(string, position, character):
+    lists = list(string)
+    lists[position] = character
+    
+    return "".join(lists)
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
