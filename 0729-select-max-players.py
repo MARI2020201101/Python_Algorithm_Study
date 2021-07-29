f3a2b04a9c0efc443611add8cb2ea375fa4d44c8
MARI <mari20201101@gmail.com>
2021-07-29 16:11:22 +0900

    Try select-max-players

---
+++ 0729-select-max-players.py
@@ -0,0 +1,38 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n=int(input())
+players = list()
+
+
+for i in range(n):
+  a,b=map(int,input().split())
+  players.append([a,b])
+
+maxcm=0
+maxcmi=0
+maxkg=0
+maxkgi=0
+
+for i,p in enumerate(players):
+  maxcm=max(p[0],maxcm)
+  if maxcm==p[0]:
+    maxcmi=i
+  maxkg=max(p[1],maxkg)
+  if maxkg==p[1]:
+    maxkgi=i
+
+
+cnt=0
+for i,p in enumerate(players):
+  if maxcmi==i or maxkgi==i:
+    cnt+=1
+  else:
+    for j in range(i,n):
+      f=False
+      if j==maxcmi or j==maxkgi:
+        continue
+      elif p[0] > players[j][0] and p[1] >players[j][1]:
+        f=True
+    if f==True:
+      cnt+=1
+print(cnt)
